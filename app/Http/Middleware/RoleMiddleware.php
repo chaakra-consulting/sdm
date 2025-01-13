9e1183cdb879153adc59e857b1111d1552d44fa4
ikiraihan <ikiraihann@gmail.com>
2025-01-13 08:50:32 +0700

    change middleware

--- app/Http/Middleware/RoleMiddleware.php
+++ app/Http/Middleware/RoleMiddleware.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use App\Models\Role;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -14,10 +15,13 @@ class RoleMiddleware
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
-    public function handle(Request $request, Closure $next, $role): Response
+    public function handle(Request $request, Closure $next, $roleSlug): Response
     {
-        if (!Auth::check() || Auth::user()->role_id != $role) {
-            return redirect()->route('login')->with('error', 'You do not have access to this page.');
+        if(!Auth::check()){
+        $role = Role::where('id',Auth::user()->role_id)->first();
+            if ($role->slug != $roleSlug) {
+                return redirect()->route('login')->with('error', 'You do not have access to this page.');
+            }
         }
 
         return $next($request);
