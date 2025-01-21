655cf90b26ccb1ba30698d52e48ed4f052c542a1
ikiraihan <ikiraihann@gmail.com>
2025-01-21 10:59:22 +0700

    refactor sidebar

--- resources/views/layouts/sidebars.blade.php
+++ resources/views/layouts/sidebars.blade.php
@@ -3,9 +3,19 @@
 
     <!-- Start::main-sidebar-header -->
     <div class="main-sidebar-header">
-        <a href="index.html" class="header-logo">
+        @if (Auth::check() && Auth::user()->role->slug == 'admin')
+        <a href="/admin/dashboard" class="header-logo">
             <img src="{{ asset('Tema/dist/assets/images/media/logo.png') }}" alt="logo" class="desktop-logo">
         </a>
+        @elseif (Auth::check() && Auth::user()->role->slug == 'admin-sdm')
+        <a href="/admin_sdm/dashboard" class="header-logo">
+            <img src="{{ asset('Tema/dist/assets/images/media/logo.png') }}" alt="logo" class="desktop-logo">
+        </a>
+        @else
+        <a href="/karyawan/dashboard" class="header-logo">
+            <img src="{{ asset('Tema/dist/assets/images/media/logo.png') }}" alt="logo" class="desktop-logo">
+        </a>
+        @endif
     </div>
     <!-- End::main-sidebar-header -->
 
@@ -229,6 +239,27 @@
                             </li>
                         </ul>
                     </li> <!-- End::slide -->
+
+                    {{-- <!-- Start::slide -->
+                    <li class="slide has-sub">
+                        <a href="javascript:void(0);"
+                            class="side-menu__item {{ request()->routeIs('users*') ? 'active' : '' }}">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="side-menu__icon" viewBox="0 0 24 24">
+                                <path d="M0 0h24v24H0z" fill="none" />
+                                <path
+                                    d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z" />
+                            </svg>
+                            <span class="side-menu__label">Management Gaji</span>
+                            <i class="fe fe-chevron-right side-menu__angle"></i>
+                        </a>
+                        <ul class="slide-menu child1">
+                            <li class="slide">
+                                <a href="/admin_sdm/gaji"
+                                    class="side-menu__item {{ request()->routeIs('/admin_sdm/gaji') ? 'active' : '' }}">Data
+                                    Gaji Karyawan</a>
+                            </li>
+                        </ul>
+                    </li> <!-- End::slide --> --}}
 
                     <!-- Start::slide__category -->
                     <li class="slide__category"><span class="category-name">General</span></li>
